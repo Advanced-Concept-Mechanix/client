4a38bd9baef48c4fe7f0d5ba4d26b79a40491a72
saisilinus <saisilinus19@gmail.com>
2020-05-31 03:45:31 -0400

    refined qr code generation

--- screens/qr.js
+++ screens/qr.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import {
     Text,
     View,
@@ -11,15 +11,48 @@ import {
 } from 'react-native';
 import styles from './style';
 import { QRCode } from 'react-native-custom-qr-codes-expo';
+import Mytextinput from '../components/mytextinput';
+import Mybutton from '../components/mybutton';
 
 export default function createQr(){
 
-    //const[qrText, ]
+    const[qrText, setQrText] = useState('change me');
+    const[qrTextHolder, setQrTextHolder] = useState('');
+
+    // useEffect(() => {
+
+    //     function generateQr(){
+    //         return(
+    //             <QRCode 
+    //             content={qrText}
+    //             logo={require('../assets/logo.png')}
+    //             />
+    //         );
+    //     }
+
+    //     generateQr()
+    // },[qrText]);
+
+    const handleChange = () => {
+        setQrText(qrTextHolder);
+    }
 
     return(
         <View style={styles.container}>
             <QRCode 
-            content='https://reactnative.com'
+                content={qrText}
+                logo={require('../assets/logo.png')}
+                ecl='H'
+            />
+            <Mytextinput
+            value={qrTextHolder}
+            placeholder={'Enter text for Qr code'}
+            //onChangeText={(qrText) => setQrText(qrText)}
+            onChangeText={(qrTextHolder) => setQrTextHolder(qrTextHolder)}
+            />
+            <Mybutton
+            title='Create'
+            customClick={handleChange}
             />
         </View>
     );
