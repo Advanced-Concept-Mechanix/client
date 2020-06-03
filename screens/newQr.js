3ff383e60758f02f9814774d041dee8d9de8b3e7
saisilinus <saisilinus19@gmail.com>
2020-06-03 06:22:48 -0400

    updated create multiple QR codes from product data

--- screens/newQr.js
+++ screens/newQr.js
@@ -7,8 +7,10 @@ import {
     TextInput,
     Image,
     Alert,
-    KeyboardAvoidingView
+    KeyboardAvoidingView,
+    FlatList
 } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import styles from './style';
 //import { QRCode } from 'react-native-custom-qr-codes-expo';
 import QRCode from 'react-native-qrcode-svg';
@@ -34,7 +36,7 @@ export default function createQr({ route }){
     const[qrText, setQrText] = useState('change me');
     const[qrTextHolder, setQrTextHolder] = useState('');
     const[qrNum, setQrNum] = useState(0);
-    const[update, setUpdate] = useState(false);
+    const[qrList, setQrList] = useState([]);
     // const[qrData, setQrData] = useState([]);
     const qrDataSet = [];
 
@@ -42,6 +44,7 @@ export default function createQr({ route }){
         if(qrNum === 0){
             alert('Please set a number!');
         }
+        let count = 0;
         //setUpdate(true);
         for(i = 1; i <= qrNum; i++){
             //let _qrdata = new QrData(item);
@@ -58,21 +61,10 @@ export default function createQr({ route }){
             await postData(url, _qrdata)
             .then(async(response) => {
                 if(response.ok){
+                    count++;
                     let data = await response.json();
                     console.log('Success:', data);
-                    Alert.alert(
-                        'Success',
-                        'Qr data successfully added',
-                        [
-                            {
-                                text: 'Ok',
-                                //onPress: () => navigation.navigate('login'),
-                            },
-                        ],
-                        { cancelable: false }
-                    );
                 }else{
-                    qrDataSet.length = 0;
                     let data = await response.json();
                     console.log('Failure:', data);
                     Alert.alert(
@@ -98,13 +90,98 @@ export default function createQr({ route }){
         // console.log(`qrNum: ${qrNum}`);
         // console.log((qrDataSet.length == qrNum).toString());
         // console.log(typeof qrDataSet.length + ' ' + typeof qrNum);
-        if(qrDataSet.length == qrNum){
+        if(count == qrNum){
+            setQrList([...qrDataSet]);
             console.log(qrDataSet);
+            Alert.alert(
+                'Success',
+                'Qr data successfully added',
+                [
+                    {
+                        text: 'Ok',
+                        onPress: () => qrDataSet.length =  0,
+                    },
+                ],
+                { cancelable: false }
+            );
+        }else{
+            alert('Products were not added!');
         }
         
     }
 
-    // useEffect(() => {
+    const handleChange = () => {
+
+        setQrText(qrTextHolder);
+
+    }
+
+    ListViewItemSeparator = () => {
+        return (
+            <View style={{ height: 0.5, width: '100%', backgroundColor: '#000' }} />
+        );
+    };
+
+    if(qrList.length === 0){
+        return(
+            <View style={styles.container}>
+                <QRCode 
+                    value={qrText}
+                    logo={require('../assets/logo.png')}
+                    ecl='H'
+                    getRef={(c) => svg = c}
+                />
+                <Mytextinput
+                value={qrTextHolder}
+                placeholder={'Enter text for Qr code'}
+                //onChangeText={(qrText) => setQrText(qrText)}
+                onChangeText={(qrTextHolder) => setQrTextHolder(qrTextHolder)}
+                />
+                <Mybutton
+                title='Create'
+                customClick={handleChange}
+                />
+                <Mytextinput
+                placeholder={'Enter number of qr codes'}
+                keyboardType="numeric"
+                onChangeText={(qrNum) => setQrNum(qrNum)}
+                />
+                <Mybutton
+                title='Generate Data'
+                customClick={getQrData}
+                />
+                {/* <Mybutton
+                title='Print'
+                customClick={print}
+                /> */}
+            </View>
+        );
+    }else{
+        return(
+            <View style={styles.container}>
+                <Text>List of QR Codes</Text>
+                <FlatList 
+                data={qrList}
+                ItemSeparatorComponent={ListViewItemSeparator}
+                renderItem={({ item }) =>
+                    <TouchableOpacity>
+                        <View style={{ backgroundColor: 'white', padding: 20 }}>
+                            <QRCode 
+                                value={JSON.stringify(item)}
+                                logo={require('../assets/logo.png')}
+                                ecl='M'
+                            />
+                        </View>
+                    </TouchableOpacity> 
+                } 
+                keyExtractor={(item) => item.UUID}
+                />
+            </View>
+        );
+    }
+}
+
+// useEffect(() => {
 
     //     getDataURL();
 
@@ -131,44 +208,3 @@ export default function createQr({ route }){
     //     setQrData(dataURL);
 
     // }
-
-    const handleChange = () => {
-
-        setQrText(qrTextHolder);
-
-    }
-
-    return(
-        <View style={styles.container}>
-            <QRCode 
-                value={qrText}
-                logo={require('../assets/logo.png')}
-                ecl='H'
-                getRef={(c) => svg = c}
-            />
-            <Mytextinput
-            value={qrTextHolder}
-            placeholder={'Enter text for Qr code'}
-            //onChangeText={(qrText) => setQrText(qrText)}
-            onChangeText={(qrTextHolder) => setQrTextHolder(qrTextHolder)}
-            />
-            <Mybutton
-            title='Create'
-            customClick={handleChange}
-            />
-            <Mytextinput
-            placeholder={'Enter number of qr codes'}
-            keyboardType="numeric"
-            onChangeText={(qrNum) => setQrNum(qrNum)}
-            />
-            <Mybutton
-            title='Generate Data'
-            customClick={getQrData}
-            />
-            {/* <Mybutton
-            title='Print'
-            customClick={print}
-            /> */}
-        </View>
-    );
-}
