0e3131b39d07a9686e83e416c4ab30cb0bb38a8e
saisilinus <saisilinus19@gmail.com>
2020-09-15 05:50:37 -0400

    modified canvas styling

--- screens/canvas.js
+++ screens/canvas.js
@@ -1,67 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Canvas, {Image as CanvasImage, Path2D, ImageData} from 'react-native-canvas';
 import handleImageData from '../functions/imageProcessing/handleImage';
 import {
-    StyleSheet, Dimensions, View, StatusBar, ScrollView, Image
+    View, Image
 } from 'react-native';
 
-const Example = ({sample, children}) => (
-    <View style={styles.example}>
-      <View style={styles.exampleLeft}>{children}</View>
-      <View style={styles.exampleRight}>
-        <Image source={sample} style={{width: 100, height: 100}} />
-      </View>
+import styles from './style';
+
+const Example = ({sample}) => (
+    <View>
+      <Image source={sample} style={styles.imageStyle} />
     </View>
   );
 
 export default function App(){
     return (
-        <View style={styles.container}>
-          <StatusBar hidden={true} />
-          <ScrollView style={styles.examples}>
+        <View style={styles.full}>
             <Example sample={require('../assets/purple-black-rect.png')}>
-              <Canvas ref={handleImageData} />
+                <Canvas ref={handleImageData} />
             </Example>
-          </ScrollView>
         </View>
     );
 }
-
-const commonStyles = StyleSheet.create({
-    full: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-    },
-    cell: {
-      flex: 1,
-      padding: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
-  
-  const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        ...commonStyles.full,
-    },
-    examples: {
-        ...commonStyles.full,
-        padding: 5,
-        paddingBottom: 0,
-    },
-    example: {
-        paddingBottom: 5,
-        flex: 1,
-        flexDirection: 'row',
-    },
-    exampleLeft: {
-        ...commonStyles.cell,
-    },
-    exampleRight: {
-        ...commonStyles.cell,
-    },
-});
--- screens/style.js
+++ screens/style.js
@@ -71,7 +71,21 @@ const styles = StyleSheet.create({
         flex: 1,
         resizeMode: "cover",
         justifyContent: "center"
-    }
+    },
+    full: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: colors.WHITE
+    },
+        cell: {
+        flex: 1,
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
   });
 
   export default styles;
